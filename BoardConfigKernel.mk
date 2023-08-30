d25daf3f84959aa17016cb12bbb14384ba24f644
Rikkaawa <3188466519@qq.com>
2023-08-30 21:01:39 +0800

    build without kernel source

--- BoardConfigKernel.mk
+++ BoardConfigKernel.mk
@@ -6,9 +6,6 @@
 
 DEVICE_KERNEL_PATH := device/xiaomi/diting-kernel
 
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8450
-TARGET_KERNEL_CONFIG := vendor/waipio_GKI.config vendor/diting_GKI.config
-
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_BINARIES := kernel
 BOARD_RAMDISK_USE_LZ4 := true
@@ -24,7 +21,7 @@ TARGET_PREBUILT_KERNEL := $(DEVICE_KERNEL_PATH)/prebuilts/kernel
 TARGET_PREBUILT_DTB := $(DEVICE_KERNEL_PATH)/prebuilts/dtb
 
 PRODUCT_COPY_FILES += \
-    $(TARGET_PREBUILT_DTB):$(TARGET_COPY_OUT)/dtb \
+    $(TARGET_PREBUILT_DTB):$(TARGET_COPY_OUT)/dtb.img \
     $(TARGET_PREBUILT_KERNEL):kernel \
     $(call find-copy-subdir-files,*,$(DEVICE_KERNEL_PATH)/ramdisk-modules/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules) \
     $(call find-copy-subdir-files,*,$(DEVICE_KERNEL_PATH)/vendor-modules/,$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules)
